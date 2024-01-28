3f96a82e67f084a3d319cb6f762e2a7afe8afa97
Aditya Mustofa Akbar <43461458+keteksemut@users.noreply.github.com>
2024-01-28 18:26:19 +0700

    asdasda

---
+++ _next/static/chunks/sdf.js
@@ -0,0 +1,42 @@
+"use strict";
+(self.webpackChunk_N_E = self.webpackChunk_N_E || []).push([
+  [8199],
+  {
+    8199: function (e, t, n) {
+      n.r(t);
+      var r,
+        a = n(959);
+      function c() {
+        return (c = Object.assign
+          ? Object.assign.bind()
+          : function (e) {
+              for (var t = 1; t < arguments.length; t++) {
+                var n = arguments[t];
+                for (var r in n)
+                  Object.prototype.hasOwnProperty.call(n, r) && (e[r] = n[r]);
+              }
+              return e;
+            }).apply(this, arguments);
+      }
+      var l = (0, a.memo)(function (e) {
+        return a.createElement(
+          "svg",
+          c(
+            {
+              viewBox: "0 0 15 15",
+              fill: "none",
+              xmlns: "http://www.w3.org/2000/svg",
+            },
+            e
+          ),
+          r ||
+            (r = a.createElement("path", {
+              d: "M3.8 14.4h-3V4.8h3v9.6Zm-1.5-11c-1 0-1.7-.6-1.7-1.6S1.4 0 2.3 0C3.3 0 4 .8 4 1.8s-.7 1.7-1.7 1.7Zm12.6 11h-3V9.2c0-1.5-.7-2-1.5-2-1 0-1.8.8-1.8 2.1v5.1h-3V4.8h2.8v1.4c.3-.7 1.3-1.6 2.8-1.6 1.8 0 3.5 1 3.5 3.9v6h.2Z",
+              fill: "black",
+            }))
+        );
+      });
+      t.default = l;
+    },
+  },
+]);
